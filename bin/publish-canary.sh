ee675f6c41ac7c87f3986c65536e1526acbdce56
Gaspard Anna Bucher <g.a.bucher@midasum.com>
2024-12-24 10:09:53 +0100

    Fix canary build

--- bin/publish-canary.sh
+++ bin/publish-canary.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -e
+
+DATE=$(date +'%Y%m%dT%H%M%S')
+
 # Set up environment
 if ! command -v pnpm &>/dev/null; then
   echo "pnpm is not installed. Please install it first."
@@ -23,14 +27,19 @@ pnpm install-all
 pnpm core build
 pnpm react build
 
-# Set canary versions
-for pkg in core react; do
-  cd packages/$pkg
-  npm --no-git-tag-version version $(npm pkg get version | sed 's/"//g')-canary.$(date +'%Y%m%dT%H%M%S')
-  cd ../..
-done
+# ================ CORE
+cd packages/core
+CORE_VERSION=$(npm pkg get version | sed 's/"//g')-canary.$DATE
+npm --no-git-tag-version version $CORE_VERSION
 
-set -e
+# ================ REACT
+cd ../react
+REACT_VERSION=$(npm pkg get version | sed 's/"//g')-canary.$DATE
+npm --no-git-tag-version version $REACT_VERSION
+npm pkg set dependencies.@tilia/core="$CORE_VERSION"
+cd ../..
+
+exit 0
 
 # Publish @tilia/core to NPM
 cd packages/core
