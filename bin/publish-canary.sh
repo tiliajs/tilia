126922ecdd3bf7dc8a8648b4272cd30bdd854852
Gaspard Anna Bucher <g.a.bucher@midasum.com>
2025-01-20 16:28:27 +0100

    Check semver versions before canary publication

--- bin/publish-canary.sh
+++ bin/publish-canary.sh
@@ -18,15 +18,35 @@ else
   echo "Repository is clean. Proceeding with the operation."
 fi
 
+is_semver() {
+  local version="$1"
+  # Regex for SemVer compliance
+  if [[ "$version" =~ ^[0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)*)?(\+[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)*)?$ ]]; then
+    echo "Valid SemVer: $version"
+    return 0
+  else
+    echo "Invalid SemVer: $version"
+    return 1
+  fi
+}
+
 # Install dependencies
 pnpm i
 # Check compilation for all projects
 pnpm core res:build
 pnpm react res:build
 
+cd core
+CORE_VERSION=$(npm pkg get version | sed 's/"//g')
+is_semver "$CORE_VERSION"
+cd ../react
+REACT_VERSION=$(npm pkg get version | sed 's/"//g')
+is_semver "$REACT_VERSION"
+cd ..
+
 # ================ CORE
 cd core
-CORE_VERSION=$(npm pkg get version | sed 's/"//g')-canary.$DATE
+CORE_VERSION=$CORE_VERSION-canary.$DATE
 npm --no-git-tag-version version $CORE_VERSION
 CANARY=true pnpm publish --tag canary --access public --no-git-checks
 
@@ -39,7 +59,7 @@ sleep 3
 
 # ================ REACT
 cd ../react
-REACT_VERSION=$(npm pkg get version | sed 's/"//g')-canary.$DATE
+REACT_VERSION=$REACT_VERSION-canary.$DATE
 npm --no-git-tag-version version $REACT_VERSION
 npm pkg set dependencies.@tilia/core="$CORE_VERSION"
 CANARY=true pnpm publish --tag canary --access public --no-git-checks
