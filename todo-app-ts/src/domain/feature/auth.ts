e8870436074b26e4bbad8ef5c078bee6589022ed
Gaspard Anna Bucher <g.a.bucher@midasum.com>
2025-05-20 10:36:34 +0200

    Fix ts todo app

--- todo-app-ts/src/domain/feature/auth.ts
+++ todo-app-ts/src/domain/feature/auth.ts
@@ -1,15 +1,15 @@
+import type { Tilia } from "tilia";
 import {
   authenticated,
   notAuthenticated,
   type Auth,
   type User,
 } from "../interface/auth";
-import { type Context } from "../model/context";
 
 /** Trivial auth adapter.
  *
  */
-export function makeAuth({ connect }: Context) {
+export function makeAuth({ connect }: Tilia) {
   const auth: Auth = connect({
     auth: notAuthenticated(),
     login: (user: User) => (auth.auth = authenticated(user)),
